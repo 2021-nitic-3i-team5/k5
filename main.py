665781ee509c411b813670ab0f6f82ab5cd4ae81
Flisan <me@loxygen.dev>
2021-10-28 11:29:31 +0900

    [編集] キャリブレーション

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ def stop(seconds_to_stop: number):
     pins.servo_write_pin(AnalogPin.P1, 90)
     wait(seconds_to_stop)
 
+def on_button_pressed_a():
+    move_liner(1, 90)
+    turn_right(90)
+    move_liner(1, 90)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def move_liner(seconds_to_move: number, speed: number):
+    global degrees2
     degrees2 = 90 - speed / 100 * 90
     pins.servo_write_pin(AnalogPin.P1, degrees2)
     pins.servo_write_pin(AnalogPin.P2, 180 - degrees2)
-    wait(seconds_to_move)
+    wait(seconds_to_move * MICRO_SECOND_IN_A_SECOND)
     stop(0)
-
 def wait(seconds_to_wait: number):
     control.wait_micros(seconds_to_wait * MICRO_SECOND_IN_A_SECOND)
-
 def turn_right(degrees: number):
-    time_to_perform = degrees * MICRO_SECOND_IN_A_SECOND / NUMBER_OF_DEGREES_PER_SEC
+    global time_to_perform
+    time_to_perform = degrees / NUMBER_OF_DEGREES_PER_SEC * MICRO_SECOND_IN_A_SECOND
     pins.servo_write_pin(AnalogPin.P1, 45)
     pins.servo_write_pin(AnalogPin.P2, 45)
     wait(time_to_perform)
     stop(0)
-
+time_to_perform = 0
+degrees2 = 0
 NUMBER_OF_DEGREES_PER_SEC = 0
 MICRO_SECOND_IN_A_SECOND = 0
-MICRO_SECOND_IN_A_SECOND = 1000000
-NUMBER_OF_DEGREES_PER_SEC = 200
+MICRO_SECOND_IN_A_SECOND = 1000
+NUMBER_OF_DEGREES_PER_SEC = 190
--- main.ts
+++ main.ts
@@ -1,30 +1,59 @@
-function stop(seconds_to_stop: number) {
+function stop (seconds_to_stop: number) {
     pins.servoWritePin(AnalogPin.P2, 90)
     pins.servoWritePin(AnalogPin.P1, 90)
     wait(seconds_to_stop)
 }
-
-function move_liner(seconds_to_move: number, speed: number) {
-    let degrees2 = 90 - speed / 100 * 90
+input.onButtonPressed(Button.A, function () {
+    move_liner(1, 90)
+    wait_with_blink(500)
+    turn_right(90)
+    wait_with_blink(500)
+    turn_right(-90)
+    wait_with_blink(500)
+    move_liner(1, 90)
+})
+function move_liner (seconds_to_move: number, speed: number) {
+    degrees2 = 90 - speed / 100 * 90
     pins.servoWritePin(AnalogPin.P1, degrees2)
     pins.servoWritePin(AnalogPin.P2, 180 - degrees2)
-    wait(seconds_to_move)
-    stop(0)
+    wait(seconds_to_move * MICRO_SECOND_IN_A_SECOND)
+    stop(1)
 }
-
-function wait(seconds_to_wait: number) {
+function wait (seconds_to_wait: number) {
     control.waitMicros(seconds_to_wait * MICRO_SECOND_IN_A_SECOND)
 }
-
-function turn_right(degrees: number) {
-    let time_to_perform = degrees * MICRO_SECOND_IN_A_SECOND / NUMBER_OF_DEGREES_PER_SEC
-    pins.servoWritePin(AnalogPin.P1, 45)
-    pins.servoWritePin(AnalogPin.P2, 45)
-    wait(time_to_perform)
-    stop(0)
+function wait_with_blink (数値: number) {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(数値)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 }
-
+function turn_right (degrees: number) {
+    time_to_perform = Math.abs(degrees) / NUMBER_OF_DEGREES_PER_SEC * MICRO_SECOND_IN_A_SECOND
+    if (degrees > 0) {
+        pins.servoWritePin(AnalogPin.P1, 135)
+        pins.servoWritePin(AnalogPin.P2, 135)
+    } else {
+        pins.servoWritePin(AnalogPin.P1, 45)
+        pins.servoWritePin(AnalogPin.P2, 45)
+    }
+    wait(time_to_perform)
+    stop(1)
+}
+let time_to_perform = 0
+let degrees2 = 0
 let NUMBER_OF_DEGREES_PER_SEC = 0
 let MICRO_SECOND_IN_A_SECOND = 0
-MICRO_SECOND_IN_A_SECOND = 1000000
-NUMBER_OF_DEGREES_PER_SEC = 200
+MICRO_SECOND_IN_A_SECOND = 1000
+NUMBER_OF_DEGREES_PER_SEC = 278
