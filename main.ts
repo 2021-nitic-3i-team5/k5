920019c51ecf1ea57961f9296080ebacd40728b2
Flisan <me@loxygen.dev>
2021-11-04 11:06:42 +0900

    feat: 挙動の改善

--- main.ts
+++ main.ts
@@ -25,18 +25,39 @@ function draw_rasen () {
     for (let 螺旋数 = 0; 螺旋数 <= 49; 螺旋数++) {
         描画時間 = 500 + 螺旋数 * 250
         for (let カウンター = 0; カウンター <= 3; カウンター++) {
-            pins.digitalWritePin(DigitalPin.P1, 60)
-            pins.digitalWritePin(DigitalPin.P2, 120)
+            basic.showNumber(カウンター)
+            pins.servoWritePin(AnalogPin.P1, 60)
+            pins.servoWritePin(AnalogPin.P2, 120)
             if (カウンター >= 2) {
                 basic.pause(描画時間)
             } else {
                 basic.pause(描画時間 + 250)
             }
+            pins.servoWritePin(AnalogPin.P1, 90)
+            pins.servoWritePin(AnalogPin.P2, 90)
+            basic.showLeds(`
+                . # # # .
+                . # . # .
+                . # # # .
+                . # # . .
+                . # . # .
+                `)
+            pins.servoWritePin(AnalogPin.P1, 45)
+            pins.servoWritePin(AnalogPin.P2, 45)
+            // 実際は 323.74101.
+            basic.pause(320)
+            pins.servoWritePin(AnalogPin.P1, 90)
+            pins.servoWritePin(AnalogPin.P2, 90)
         }
-        pins.digitalWritePin(DigitalPin.P1, 45)
-        pins.digitalWritePin(DigitalPin.P1, 45)
-        // 実際は 323.74101.
-        basic.pause(300)
+        pins.servoWritePin(AnalogPin.P1, 90)
+        pins.servoWritePin(AnalogPin.P2, 90)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
     }
 }
 function draw_rasen_shape (points: number) {
